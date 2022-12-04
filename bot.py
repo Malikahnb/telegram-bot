45619e997fa5b596d2b38f9e135587f470970b85
Malikahnb <hasanboyevabudur@gmail.com>
2022-12-04 22:03:29 +0500

    added functions, not working

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import telebot
-from telebot import types
 
 bot = telebot.TeleBot('5626502754:AAG4vkxqLA4WBe8LUEpxSTQ79UZq4G9N-AE')
 
@@ -15,23 +14,10 @@ def start(message):
     bot.send_message(message.chat.id, name, parse_mode='html')
 
 
-# @bot.callback_query_handler(func=lambda call: True)
-# def callback_worker(call):
-#     if call.data == "add":
-#         add(first_num())
-#     elif call.data == "subtract":
-#         bot.send_message(call.message.chat.id, 'Failed')
-
-
 def first_num(message):  # asking for the input
     global first
     first = message.text
     bot.send_message(message.from_user.id, f'Please send the <b>first</b> number: ', parse_mode='html')
-    # while first == 0:
-    #     try:
-    #         first = int(message.text)
-    #     except Exception:
-    #         bot.send_message(message.from_user.id, f'<b>Please send correct number!</b>', parse_mode='html')
     bot.register_next_step_handler(message, second_num)
 
 
@@ -39,11 +25,6 @@ def second_num(message):  # asking for the input
     global second
     second = message.text
     bot.send_message(message.from_user.id, f'Please send the <b>second</b> number: ', parse_mode='html')
-    # while second == 0:
-    #     try:
-    #         second = int(message.text)
-    #     except Exception:
-    #         bot.send_message(message.from_user.id, f'<b>Please send correct number!</b>', parse_mode='html')
     bot.register_next_step_handler(message, operations)
 
 
@@ -57,7 +38,7 @@ def add(message):
 
 
 @bot.message_handler(commands=['subtract'])
-def add(message):
+def subtract(message):
     first_num(message)
     global result
     if message == '/subtract':
@@ -66,7 +47,7 @@ def add(message):
 
 
 @bot.message_handler(commands=['multiply'])
-def add(message):
+def multiply(message):
     first_num(message)
     global result
     if message == '/multiply':
@@ -75,7 +56,7 @@ def add(message):
 
 
 @bot.message_handler(commands=['divide'])
-def add(message):
+def divide(message):
     first_num(message)
     global result
     if message == '/divide':
@@ -85,21 +66,16 @@ def add(message):
 
 @bot.message_handler(commands=['add', 'subtract', 'multiply', 'divide'])
 def operations(message):
-    while first == '':
-        if message.text == '/add':
-            bot.send_message(message.from_user.id, f'<i>Answer:</i>{first} + {second}', parse_mode='html')
-        elif message.text == '/subtract':
-            bot.send_message(message.from_user.id, f'<i>Answer:</i>{first} - {second}', parse_mode='html')
-        elif message.text == '/multiply':
-            bot.send_message(message.from_user.id, f'<i>Answer:</i>{first} * {second}', parse_mode='html')
-        elif message.text == '/divide':
-            bot.send_message(message.from_user.id, f'<i>Answer:</i>{first} // {second}', parse_mode='html')
-        else:
-            bot.send_message(message.from_user.id, 'Please enter the correct operation', parse_mode='html')
-            break
-        first_num(message)
+    if message.text == '/add':
+        bot.send_message(message.from_user.id, add(message), parse_mode='html')
+    elif message.text == '/subtract':
+        bot.send_message(message.from_user.id, subtract(message), parse_mode='html')
+    elif message.text == '/multiply':
+        bot.send_message(message.from_user.id, multiply(message), parse_mode='html')
+    elif message.text == '/divide':
+        bot.send_message(message.from_user.id, divide(message), parse_mode='html')
+    else:
+        bot.send_message(message.from_user.id, 'Please enter the correct operation', parse_mode='html')
 
 
 bot.polling(none_stop=True)
-
-# link to the source: https://habr.com/ru/post/442800/
